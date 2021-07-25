5f9bcf1d1bf7493a5740ee3efa0e4b53fa9c7888
dennis <85466642+dennis9352@users.noreply.github.com>
2021-07-25 18:51:20 +0900

    basic structure

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import router from "./routes/index.js";
+import dotenv from 'dotenv'
+import "./models/index.js";
+
+dotenv.config()
+const app = express();
+
+const corsOption = {
+    origin: process.env.CORS,
+    Credential: true,
+    optionSuccessStatus: 200,
+  };
+
+app.use(cors(corsOption));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", router);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("서버 연결 성공");
+});
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import aRouter from "./a.js";
+import bRouter from "./b.js";
+import cRouter from "./c.js";
+
+const router = express.Router();
+
+router.use("/a", aRouter);
+router.use("/b", bRouter);
+router.use("/c", cRouter);
+
+
+
+export default router;
