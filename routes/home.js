0901aae0bf852594af93588d03db98be50da448f
Parkjinhong <duabck@naver.com>
2021-07-29 17:10:31 +0900

    검색결과 상세페이지, favorite

--- routes/home.js
+++ routes/home.js
@@ -8,8 +8,8 @@ const router = express.Router();
 router.get("/search/:keyword", async (req, res) => {
   try{    
       const keyword = decodeURIComponent(req.params.keyword);
-      const nameKey = new RegExp(keyword) 
-
+      const nameKey = new RegExp(keyword)
+     
       let food = await Food.find({$text: {$search: nameKey}},
         { score: {$meta: "textScore"}}).sort({socre:{$meta: "textScore"}})
       
@@ -37,6 +37,32 @@ router.get("/search/:keyword", async (req, res) => {
 })
 
 //검색결과 상세페이지(음식) API
+
+router.get("/search/detail/:foodId", async (req, res) => {
+  try{
+    const {foodId} = req.params
+    const foodDetail = await Food.findOne({_id : foodId}).exec();
+    
+    const name = foodDetail['name']
+    const calorie = foodDetail['kcal']
+    const gram = foodDetail['forOne']
+
+    res.send({
+      name: name,
+      calorie: calorie,
+      gram: gram,
+    })
+   
+  }catch(err){
+    console.log(err) 
+    res.status(400).send({
+      "errorMessage": "상세보기 조회중 에러발생"
+    })
+    return;
+  }
+  
+  
+})
 
 
 
