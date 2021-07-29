6b7ee3eb586cec08d289f4ff5de6fad277232c2f
dennis <85466642+dennis9352@users.noreply.github.com>
2021-07-29 17:11:44 +0900

    mongodb connect

--- models/index.js
+++ models/index.js
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose'
+import dotenv from "dotenv"
 
+
+dotenv.config()
 const connect = () => {
 	if (process.env.DEBUG !== 'false') {
 		mongoose.set('debug', true)
 	}
 }
+const IP = process.env.IPADDRESS
+const dbId = process.env.DBID
+const dbPw = process.env.DBPW
 
 mongoose.connect('mongodb://localhost:27017/calories', {
 	useNewUrlParser: true,
@@ -14,6 +20,21 @@ mongoose.connect('mongodb://localhost:27017/calories', {
 	if (err) console.error('MongoDB 에러', err)
 	else console.log('MongoDB 연결 성공')
 })
+
+mongoose.connection.on('error', err => {
+	console.error('MongoDB 연결 에러', err)
+	connect()
+})
+
+// mongoose.connect(`mongodb://${dbId}:${dbPw}@${IP}:27017/admin`, {
+// 	dbName: 'calories',
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// 	useCreateIndex: true,
+// }, err => {
+// 	if (err) console.error('MongoDB 에러', err)
+// 	else console.log('MongoDB 연결 성공')
+// })
 
 mongoose.connection.on('error', err => {
 	console.error('MongoDB 연결 에러', err)
