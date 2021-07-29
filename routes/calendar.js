6ce3b3a9182fe31673712dfc03c9146733731898
dennis <85466642+dennis9352@users.noreply.github.com>
2021-07-30 01:53:38 +0900

    dashboard added
    showing today's calories in dashboard

--- routes/calendar.js
+++ routes/calendar.js
@@ -2,6 +2,21 @@ import express from "express";
 
 const router = express.Router();
 
+router.get('/dash', async(req, res) => {
+    const {date} = req.body;
+    //const { userId } = res.locals.userId
+    const year = date.split('-')[0]
+    const month = date.split('-')[1]
+    const day = date.split('-')[2]
+
+    const dash = Record.find(
+        {
+            $and : [{ userId : userId }, { year : year }, { month : month }, { day: day }]
+        }).exec()
+    
+    res.status(200).json({ dash })
+})
+
 router.get('/', async(req, res) => {
     const { date } = req.body;
     //const { userId } = res.locals.userId
