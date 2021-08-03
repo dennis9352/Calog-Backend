d7c99955b04c2c0efb704ca7d249a8508ef13c75
Parkjinhong <duabck@naver.com>
2021-08-03 21:45:44 +0900

    user 스케마에 bmr 추가, 바디스펙 수정하기
    user 스케마에 bmr 추가, 바디스펙 수정하기

--- models/users.js
+++ models/users.js
@@ -14,6 +14,7 @@ export const userSchema = new mongoose.Schema({
     height: { type: Number},
     age: { type: Number},
     control: { type: String},
+    bmr: {type: Object},
     foodFavorites: {type: mongoose.Schema.Types.ObjectId,},
     records: {type: mongoose.Schema.Types.ObjectId,},
 });
