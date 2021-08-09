f17d84751b86ba31395076b3be3c9244c045fba0
Parkjinhong <duabck@naver.com>
2021-08-09 21:35:25 +0900

    mongodb admin으로

--- models/index.js
+++ models/index.js
@@ -11,19 +11,7 @@ const IP = process.env.IPADDRESS
 const dbId = process.env.DBID
 const dbPw = process.env.DBPW
 
-mongoose.connect('mongodb://localhost:27017/calories', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
-}, err => {
-	if (err) console.error('MongoDB 에러', err)
-	else console.log('MongoDB 연결 성공')
-})
-
-
-
-// mongoose.connect(`mongodb://${dbId}:${dbPw}@${IP}:27017/admin`, {
-// 	dbName: 'calories',
+// mongoose.connect('mongodb://localhost:27017/calories', {
 // 	useNewUrlParser: true,
 // 	useUnifiedTopology: true,
 // 	useCreateIndex: true,
@@ -31,6 +19,18 @@ mongoose.connect('mongodb://localhost:27017/calories', {
 // 	if (err) console.error('MongoDB 에러', err)
 // 	else console.log('MongoDB 연결 성공')
 // })
+
+
+
+mongoose.connect(`mongodb://${dbId}:${dbPw}@${IP}:27017/admin`, {
+	dbName: 'calories',
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+}, err => {
+	if (err) console.error('MongoDB 에러', err)
+	else console.log('MongoDB 연결 성공')
+})
 
 
 mongoose.connection.on('error', err => {
